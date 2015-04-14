da8e517517eed461e983637838c1e1fc1e161a89
Howard M. Harte <hharte@cyngn.com>
2015-04-14 11:35:57 -0700

    fugu: do not use 2ndbootloader.mk for Moorefield
    Fugu has its own makefile in hardware/intel/bootstub to generate
    the 2ndbootloader.  Don't use the one here.
    
    Change-Id: I6ae69b0d30264fb1e244fb91db92bc164ae9f5e5

--- tasks/2ndbootloader.mk
+++ tasks/2ndbootloader.mk
@@ -1,4 +1,9 @@
+# Moorefield is handled by hardware/intel/bootstub/
+ifneq ($(TARGET_BOARD_PLATFORM),moorefield)
+
 LOCAL_DTB_PATH := $(LOCAL_KERNEL_PATH)/$(BOARD_DTB_FILE)
 
 $(INSTALLED_2NDBOOTLOADER_TARGET): $(LOCAL_DTB_PATH) | $(ACP)
 	$(hide) $(ACP) -fp $(LOCAL_DTB_PATH) $@
+
+endif
